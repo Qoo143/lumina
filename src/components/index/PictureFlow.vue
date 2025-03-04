383755dd3436158348752b03cd17826551d9c96d
Qoo143 <wayne676803@gmail.com>
2025-03-04 10:29:08 +0800

    給予測試圖片

--- src/components/index/PictureFlow.vue
+++ src/components/index/PictureFlow.vue
@@ -1,5 +1,62 @@
 <template>
-  <div class="bg-gray-200 w-full h-full">
+  <div class=" w-full h-full my-0 mx-auto bg-gray-200 ">
+
+    <!-- 這是創建圖片卡片 -->
+    <div v-for="item in 5">
+      <div class="flowCard">
+        <div>
+          <img src="@/assets/flow/flow01.jpg" alt="">
+        </div>
+      </div>
+      <div class="flowCard">
+        <div>
+          <img src="@/assets/flow/flow02.jpg" alt="">
+        </div>
+      </div>
+      <div class="flowCard">
+        <div>
+          <img src="@/assets/flow/flow03.jpg" alt="">
+        </div>
+      </div>
+      <div class="flowCard">
+        <div>
+          <img src="@/assets/flow/flow04.jpg" alt="">
+        </div>
+      </div>
+      <div class="flowCard">
+        <div>
+          <img src="@/assets/flow/flow05.jpg" alt="">
+        </div>
+      </div>
+      <div class="flowCard">
+        <div>
+          <img src="@/assets/flow/flow06.jpg" alt="">
+        </div>
+      </div>
+      <div class="flowCard">
+        <div>
+          <img src="@/assets/flow/flow07.jpg" alt="">
+        </div>
+      </div>
+      <div class="flowCard">
+        <div>
+          <img src="@/assets/flow/flow08.jpg" alt="">
+        </div>
+      </div>
+      <div class="flowCard">
+        <div>
+          <img src="@/assets/flow/flow09.jpg" alt="">
+        </div>
+      </div>
+      <div class="flowCard">
+        <div>
+          <img src="@/assets/flow/flow10.jpg" alt="">
+        </div>
+      </div>
+    </div>
+
+    <!-- 清除浮動 -->
+    <div class="clear-both"></div>
 
   </div>
 </template>
